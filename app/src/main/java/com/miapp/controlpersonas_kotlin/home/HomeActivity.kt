0c70c9f55769e7a853d1dbf7b831cab627ce7db5
andresesposito62 <77408396+andresesposito62@users.noreply.github.com>
2021-04-28 18:45:33 -0500

    visual implementation of action select spinner
    visual implementation of action select spinner
    
    Co-Authored-By: Andrés Espósito <77408396+andresesposito62@users.noreply.github.com>

--- app/src/main/java/com/miapp/controlpersonas_kotlin/home/HomeActivity.kt
+++ app/src/main/java/com/miapp/controlpersonas_kotlin/home/HomeActivity.kt
@@ -2,11 +2,20 @@ package com.miapp.controlpersonas_kotlin.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.miapp.controlpersonas_kotlin.R
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var spinner : Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        spinner = findViewById<Spinner>(R.id.spinner_for_action_selection)
+        val spinnerAdapter : ArrayAdapter<CharSequence> = ArrayAdapter
+            .createFromResource(this, R.array.options_spinner, android.R.layout.simple_spinner_item)
+        spinner.adapter = spinnerAdapter
     }
 }
