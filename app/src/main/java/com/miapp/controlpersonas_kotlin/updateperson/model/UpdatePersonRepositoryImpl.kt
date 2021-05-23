c11aaa2c7006f854d6250dc257fbde57f8e8dc26
andresesposito62 <77408396+andresesposito62@users.noreply.github.com>
2021-05-23 13:44:25 -0500

    update data with room is implemented

--- app/src/main/java/com/miapp/controlpersonas_kotlin/updateperson/model/UpdatePersonRepositoryImpl.kt
+++ app/src/main/java/com/miapp/controlpersonas_kotlin/updateperson/model/UpdatePersonRepositoryImpl.kt
@@ -1,6 +1,9 @@
 package com.miapp.controlpersonas_kotlin.updateperson.model
 
 import android.content.Context
+import android.util.Log
+import com.miapp.controlpersonas_kotlin.databasemanager.PersonRoomEntity
+import com.miapp.controlpersonas_kotlin.databasemanager.PersonsDb
 import com.miapp.controlpersonas_kotlin.modelo.datos.InterfaceUpdateDatabaseRegistrer
 import com.miapp.controlpersonas_kotlin.modelo.domain.Persona
 import java.lang.NullPointerException
@@ -29,6 +32,32 @@ class UpdatePersonRepositoryImpl : UpdatePersonRepository {
                 statusQuery = false
             }
             return statusQuery
+        }
+    }
+
+    override suspend fun updateRegisterPersonDbRoom(person: Persona?, context: Context): Boolean? {
+
+        var result = false
+
+        try {
+            val personDao = (PersonsDb.getDatabase(context.applicationContext)).personDao()
+            val personRoom= personDao.getById(person!!.getIdentificacion().toString())
+
+            personRoom.indentification = person!!.getIdentificacion().toString()
+            personRoom.names = person!!.getNombres().toString()
+            personRoom.surnames = person!!.getApellidos().toString()
+            personRoom.phone = person!!.getTelefono().toString()
+            personRoom.temperature = person!!.getTemperatura().toString()
+            personRoom.rol = person!!.getRol().toString()
+
+            personDao.update(personRoom)
+            result = true
+
+        }catch (e : NullPointerException){
+            result = false
+        }
+        finally {
+            return result
         }
     }
 }
